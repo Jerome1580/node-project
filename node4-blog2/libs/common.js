298676d67c523310efd9c21f68127cf1a47f3df1
raoju <raoju1580@qq.com>
2017-11-09 17:08:27 +0800

    node-blog项目，管理后台，新增登录权限控制；session保存登录状态；后台banner管理新增功能，删除功能；管理后台模板ejs把公共部分提出来

--- node4-blog2/libs/common.js
+++ node4-blog2/libs/common.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 module.exports={
     MD5_SUFFIX:'ffsfkee42374$^$@#$@@fsdfFWEFF%$#%#()}{}}FDS@SD^GADss大师',
     md5:function(str){
-        var obj = crypto.crypto.createHash('md5'); //使用md5加密
+        var obj = crypto.createHash('md5'); //使用md5加密
 
         obj.update(str); // 传入字符串
 
--- node4-blog2/route/admin.js
+++ node4-blog2/route/admin.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const common=require('../libs/common');
+const mysql=require('mysql');
+
+var db=mysql.createPool({host:'localhost',user:'root',password:'123456',database:'learn'})
 
 module.exports=function(){
 
@@ -16,11 +19,88 @@ module.exports=function(){
 
     })
 
-    //
-    router.use('/login',function(req,res){
-        console.log(req.body)
+    //单纯访问页面
+    router.get('/login',function(req,res){
         res.render('admin/login.ejs')
     })
+
+    // 提交用户名密码
+    router.post('/login',function(req,res){
+        var username = req.body.username;
+        var password = common.md5(req.body.password + common.MD5_SUFFIX);
+
+        db.query(`SELECT * FROM admin_table where username = '${username}'`,(err,data)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send('datase error').end()
+            }else{
+                if(data.length == 0 ){
+                    res.status(400).send('no this admin').end()
+                }else{
+                    if(data[0].password == password){
+                        // 成功
+                        req.session['admin_id']= data[0].ID; // 赋值session
+                        res.redirect('/admin/')
+                    }else{
+                         res.status(400).send('this password is incorrect').end()
+                    }
+                }
+            }
+        })
+
+    })
+
+    router.get('/',(req,res)=>{
+        res.render('admin/index.ejs',{})
+    })
+
+    router.get('/banners',(req,res)=>{
+
+        switch(req.query.act){
+            case 'mod':
+                break;
+            case 'del':
+                db.query(`DELETE FROM banner_table WHERE ID='${req.query.id}'`,(err,data)=>{
+                    if(err){
+                        console.log(err);
+                        res.status(500).send('database error').end()
+                    }else{
+                        res.redirect('/admin/banners')
+                    }
+                })
+                break;
+            default:
+                db.query(`SELECT * FROM banner_table`,(err,data)=>{
+                    if(err){
+                        console.log(err);
+                        res.status(500).send('database error').end()
+                    }else{
+                        res.render('admin/banners.ejs',{banners:data})
+                    }
+                })
+
+        }
+    })
+
+    router.post('/banners',(req,res)=>{
+        var title = req.body.title;
+        var description = req.body.description;
+        var href = req.body.href;
+
+        if(!title || !description || !href){
+            res.status(400).send('arg error').end()
+        }else{
+            db.query(`INSERT INTO banner_table (title,description,href) VALUE ('${title}','${description}','${href}')`,(err,data)=>{
+                if(err){
+                     res.status(500).send('database error').end()
+                }else{
+                    res.redirect('/admin/banners')
+                }
+            })
+        }
+    })
+
+
 
     return router
 }
